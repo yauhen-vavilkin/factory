8122ff5f4f6b41f8c20a63339296929e528a8c80
okuzminov <oleksii_kuzminov@epam.com>
2026-07-19 16:37:10 +0200

    Remove unused import

--- factory-app/src/test/java/org/folio/factory/app/web/DashboardUiControllerTest.java
+++ factory-app/src/test/java/org/folio/factory/app/web/DashboardUiControllerTest.java
@@ -32,7 +32,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
